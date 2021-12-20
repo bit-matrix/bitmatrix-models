c016c48fe5e91d92be162f6cba1177fd2b0fa941
ayseren <ayse.eren@windowslive.com>
2021-12-21 00:17:48 +0300

    CallData file import values changed

--- src/CallData.ts
+++ src/CallData.ts
@@ -1,13 +1,6 @@
-import { CALL_METHOD } from "./CALL_METHOD";
+import { CallDataBase } from "./CallDataBase";
+import { CallDataValue } from "./CallDataValue";
 
-export type CallData = {
-    method: CALL_METHOD;
-    recipientPublicKey: string;
-    slippageTolerance: string; // number; TODO
-    orderingFee: number; // number; TODO
-    value: {
-      quote: number;
-      token: number;
-      lp: number;
-    };
-  };
+export type CallData =  CallDataBase & {
+  value: CallDataValue;
+};
