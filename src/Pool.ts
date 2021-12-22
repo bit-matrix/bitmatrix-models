9089fe83a90519fd5abf42b46dd72e8dbd312bd7
Penguen Umut <uuisik@gmail.com>
2021-12-22 16:26:10 +0300

    pool model desc

--- src/Pool.ts
+++ src/Pool.ts
@@ -2,36 +2,53 @@ import { BmBlockInfo } from "./BmBlockInfo";
 import { BmTxInfo } from "./BmTxInfo";
 import { PAsset } from "./PAsset";
 
+/**
+ * worker looks before creating new pool tx:
+ * if(synced && lastSentPtx === undefined && exist(new_ctx))
+ */
 export type Pool = {
   /**
-   * worker looks before creating new pool tx:
-   * if(synced && lastSentPtx === undefined && exist(new_ctx))
+   * pool assets, values
    */
   id: string;
   quote: PAsset;
   token: PAsset;
   lp: PAsset;
 
-  // pool creation tx info
+  /**
+   * pool creation tx info
+   */
   initialTx: BmTxInfo;
 
-  // last worker checked block info
+  /**
+   * last worker checked block info
+   */
   lastSyncedBlock: BmBlockInfo;
 
-  // recent block height on network
+  /**
+   * recent block height on network
+   */
   bestBlockHeight: number;
 
-  // lastSyncedBlock.height === bestBlockHeight
-  // (if true worker can create pool tx else pass creation pool tx)
+  /**
+   * lastSyncedBlock.height === bestBlockHeight
+   * (if true worker can create pool tx else pass creation pool tx)
+   */
   synced: boolean;
 
-  // recent worker found pool tx (it may be spent, validate "synced")
+  /**
+   * recent worker found pool tx (it may be spent, validate "synced")
+   */
   lastUnspentTx: BmTxInfo;
 
-  // if worker broadcast one tx, save here.
-  // when it confirmed (worker found new ptx is equal to this), delete for new creation pool tx
+  /**
+   * if worker broadcast one tx, save here.
+   * when it confirmed (worker found new ptx is equal to this), delete for new creation pool tx
+   */
   lastSentPtx: string | undefined;
 
-  // pool is active
+  /**
+   * pool is active
+   */
   active: boolean;
 };
