19f022a698178a29337ab842ebd5ee788a6feb05
reyhanboydag <reyhanboydag@gmail.com>
2022-07-06 15:45:55 +0300

    dist

---
+++ dist/CTXPTXResult.d.ts
@@ -0,0 +1,18 @@
+export declare type CTXPTXResult = {
+    user_supply_total: number;
+    user_supply_lp_fees: number;
+    user_supply_available: number;
+    constant_coefficient: number;
+    constant_coefficient_downgraded: number;
+    payout_additional_fees: number;
+    new_pair_2_pool_liquidity_apx_1: number;
+    new_pair_2_pool_liquidity_apx_2: number;
+    new_pair_1_pool_liquidity_apx_1?: number;
+    new_pair_1_pool_liquidity_apx_2?: number;
+    user_received_pair_2_apx: number;
+    user_received_pair_1_apx?: number;
+    user_received_pair_2: number;
+    user_received_pair_1?: number;
+    new_pool_pair_1_liquidity: number;
+    new_pool_pair_2_liquidity: number;
+};
