6e6780dfa4c18e08ce39b62152a778cd2078d523
reyhanboydag <reyhanboydag@gmail.com>
2022-07-08 16:34:37 +0300

    added CTXFinderResult model

---
+++ src/CTXFinderResult.ts
@@ -0,0 +1,28 @@
+import { ChangeOutputFinal } from "./ChangeOutputFinal";
+import { Pool } from "./Pool";
+import { SeperatedChangeOutputs } from "./SeperatedChangeOutputs";
+import { TxDetail } from "./TxDetail";
+import { TxVOutRPC } from "./TxVOutRPC";
+
+export type CTXFinderResult = {
+  outputs: TxVOutRPC[];
+  nsequenceValue: string;
+  cmtTxInOutpoints: string[];
+  cmtOutput1Value: string;
+  output2PairValue: string;
+  cmtOutput2Value: string;
+  cmtOutput3Value?: string;
+  cmtOutputFeeHexValue: string;
+  cmtOutput3PairValue: string;
+  cmtOutput3Asset?: string;
+  changeOutputFinal: ChangeOutputFinal[];
+  seperatedChangeOutputs: SeperatedChangeOutputs[];
+  methodCall: string;
+  slippageTolerance: string;
+  orderingFee: string;
+  tapTweakedResultPrefix: string;
+  cmtOutput2DecimalValue: number;
+  poolId: string;
+  pool: Pool;
+  transaction: TxDetail;
+};
