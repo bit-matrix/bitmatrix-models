3178cf978a17c59678b0fa15668f9ad0721f3d15
afarukcali <afarukcali@gmail.com>
2022-07-30 00:07:46 +0300

    dist

--- dist/Pool.d.ts
+++ dist/Pool.d.ts
@@ -1,4 +1,3 @@
-import { BmBlockInfo } from "./BmBlockInfo";
 import { BmTxInfo } from "./BmTxInfo";
 import { PAsset } from "./PAsset";
 /**
@@ -17,28 +16,7 @@ export declare type Pool = {
      * pool creation tx info
      */
     initialTx: BmTxInfo;
-    /**
-     * last worker checked block info
-     */
-    lastSyncedBlock: BmBlockInfo;
-    /**
-     * recent block height on network
-     */
-    bestBlockHeight: number;
-    /**
-     * lastSyncedBlock.height === bestBlockHeight
-     * (if true worker can create pool tx else pass creation pool tx)
-     */
-    synced: boolean;
-    /**
-     * recent worker found pool tx (it may be spent, validate "synced")
-     */
-    unspentTx?: BmTxInfo;
-    /**
-     * if worker broadcast one tx, save here.
-     * when it confirmed (worker found new ptx is equal to this), delete for new creation pool tx
-     */
-    lastSentPtx: string | undefined;
+    lastStateTxId: string;
     /**
      * pool is active
      */
