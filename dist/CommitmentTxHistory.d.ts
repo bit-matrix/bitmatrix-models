3dd40119802df77ea8e81ab78214344e3b860bec
reyhanboydag <reyhanboydag@gmail.com>
2022-08-08 12:33:43 +0300

    dist

--- dist/CommitmentTxHistory.d.ts
+++ dist/CommitmentTxHistory.d.ts
@@ -1,6 +1,7 @@
+import { CALL_METHOD } from "./CALL_METHOD";
 export declare type CommitmentTxHistory = {
     poolId: string;
-    method: string;
+    method: CALL_METHOD;
     txId: string;
     isSuccess: boolean;
     failReason?: string;
