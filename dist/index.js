2aa11c9e8aa2c748be8ce83e6859f6b118d3d16d
afarukcali <afarukcali@gmail.com>
2022-08-11 18:47:47 +0300

    dist

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.CALL_METHOD = void 0;
+exports.TX_STATUS = exports.CALL_METHOD = void 0;
 const CALL_METHOD_1 = require("./CALL_METHOD");
 Object.defineProperty(exports, "CALL_METHOD", { enumerable: true, get: function () { return CALL_METHOD_1.CALL_METHOD; } });
+const TX_STATUS_1 = require("./TX_STATUS");
+Object.defineProperty(exports, "TX_STATUS", { enumerable: true, get: function () { return TX_STATUS_1.TX_STATUS; } });
 //# sourceMappingURL=index.js.map
