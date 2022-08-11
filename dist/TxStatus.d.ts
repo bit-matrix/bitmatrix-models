b12f24e3a21b8032f06e04cae70e368100b98019
afarukcali <afarukcali@gmail.com>
2022-08-11 18:51:42 +0300

    v 0.1.31

--- dist/TxStatus.d.ts
+++ dist/TxStatus.d.ts
@@ -1,6 +1,7 @@
+import { TX_STATUS } from "./TX_STATUS";
 export declare type TxStatus = {
     txId: string;
     poolTxId: string;
-    status: TxStatus;
+    status: TX_STATUS;
     errorMessages?: string;
 };
