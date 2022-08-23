836dd3555c3eeb4f553ca9d38df64d68f4693a5a
afarukcali <afarukcali@gmail.com>
2022-08-23 16:06:46 +0300

    unused imports.

--- src/CTXFinderResult.ts
+++ src/CTXFinderResult.ts
@@ -1,17 +1,14 @@
 import WizData from "@script-wiz/wiz-data";
-import { ChangeOutputFinal } from "./ChangeOutputFinal";
-import { Pool } from "./Pool";
-import { SeperatedChangeOutputs } from "./SeperatedChangeOutputs";
 import { TxDetail } from "./TxDetail";
 import { TxVInRPC } from "./TxVInRPC";
 import { TxVOutRPC } from "./TxVOutRPC";
 
 export type CTXFinderResult = {
-  tweakKeyPrefix:string;
-  part1:string;
-  part2:string;
-  part3:string;
-  locktimeHex:string;
+  tweakKeyPrefix: string;
+  part1: string;
+  part2: string;
+  part3: string;
+  locktimeHex: string;
   outputCount: WizData;
   inputs: TxVInRPC[];
   outputs: TxVOutRPC[];
