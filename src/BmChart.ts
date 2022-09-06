e754620eda22f1b6a51a7272a7d02f0bd720835b
afarukcali <afarukcali@gmail.com>
2022-09-06 16:02:50 +0300

    Remove call data from BMChart.

--- src/BmChart.ts
+++ src/BmChart.ts
@@ -1,10 +1,7 @@
-import { CALL_METHOD } from "./CALL_METHOD";
-
 export type BmChart = {
-    time: number;
-    ptxid: string;
-    value: { quote: number; token: number; lp: number };
-    volume: { quote: number; token: number };
-    price: number;
-    method: CALL_METHOD;
+  time: number;
+  ptxid: string;
+  value: { quote: number; token: number; lp: number };
+  volume: { quote: number; token: number };
+  price: number;
 };
