f80f45a08e41ff40c4fe502d83a5a5d4f2d9fd80
afarukcali <afarukcali@gmail.com>
2022-09-07 16:42:28 +0300

    Asset model updated.

--- dist/Asset.d.ts
+++ dist/Asset.d.ts
@@ -1,4 +1,4 @@
 export declare type Asset = {
     assetHash: string;
-    name: string;
+    value: number;
 };
--- src/Asset.ts
+++ src/Asset.ts
@@ -1,4 +1,4 @@
 export type Asset = {
   assetHash: string;
-  name: string;
+  value: number;
 };
