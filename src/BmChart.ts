53dfb539621335709bae0109285210c1ba06743f
afarukcali <afarukcali@gmail.com>
2022-09-21 18:58:36 +0300

    BmChart  updated.

--- src/BmChart.ts
+++ src/BmChart.ts
@@ -2,7 +2,6 @@ export type BmChart = {
   time: number;
   ptxid: string;
   value: { quote: number; token: number; lp: number };
-  volume: { quote: number; token: number };
   price: number;
   lpFeeTier: number;
 };
